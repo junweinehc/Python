89b3f7ab323c68ca60fddd6f3770fa02f0f45612
junweinehc <74738086+junweinehc@users.noreply.github.com>
2021-01-27 23:25:43 -0800

    Add files via upload

---
+++ jusPractice.py
@@ -0,0 +1,30 @@
+
+#just practice
+#don’t waste time reading pls
+
+name = "Loha hahaha"
+print(name.title())
+
+print("python \nC \nJava")
+print(name.rstrip())
+print(name.lstrip())
+print(name.strip())
+
+fName="eric"
+lname="dak"
+fullName=f"{fName} {lName}"
+print("Hello " + fullName + ", " + "would you like to learn some Python today?")
+
+import this
+
+fp = 0.0
+while fp <= 1.0:
+    fp = fp + 0.1
+    print(fp)
+
+bicyles = ['trek','cannondale','redline','specialized']
+print(bicyles[-1])
+
+bicyles = ['trek','cannondale','redline','specialized']
+bicyles.insert(0, 'Ducati')
+print(bicyles)
